e32039d0e120bde66d4bde016aed438cf07309f9
rodrigocitadin <citadinrodrigo@gmail.com>
2025-07-10 17:33:16 -0300

    hashmaps

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -29,10 +29,16 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
+        match fruit {
+            Fruit::Apple | Fruit::Mango | Fruit::Lychee => continue,
+            fruit => basket.insert(fruit, 1),
+        };
+
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
     }
+
 }
 
 fn main() {
